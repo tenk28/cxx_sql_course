fc931d5af850058179eb4598a0eb15efbfd57911
Dmytro Shtrikker <shtrikker28@gmail.com>
2024-01-29 15:04:22 +0200

    extra main

---
+++ c_cxx/lesson1/main copy.c
@@ -0,0 +1,23 @@
+#include <stdio.h> // printf, scanf (iostream)
+#include <math.h> // all mathes functions
+
+double func(double x, double y) {
+    double num = 3.5 * pow(x, 2) + 1;
+    double den = cos(2 * y);
+    return (log(x) + num / den);
+}
+
+int main() {
+    // std::cout -> printf
+    // std::cin -> scanf
+    double x, y;
+
+    printf("Enter x: ");
+    scanf("%lf", &x);
+
+    printf("Enter y: ");
+    scanf("%lf", &y);
+
+    printf("Result: %lf\n", func(x, y));
+    return 0;
+}
