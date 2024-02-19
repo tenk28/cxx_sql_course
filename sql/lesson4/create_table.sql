e1c57839e6ef2707884250c6f5db28e2395ea8a9
Dmytro Shtrikker <shtrikker28@gmail.com>
2024-02-19 17:06:29 +0200

    add classwork sql 19.02

---
+++ sql/lesson4/create_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Shtrikker (
+    name VARCHAR(50),
+    mark INT,
+    mark_date VARCHAR(50)
+);
---
+++ sql/lesson4/insert_into.sql
@@ -0,0 +1,7 @@
+-- INSERT INTO Shtrikker(name, mark, mark_date)
+-- VALUES
+-- ('Dmytro', 12, '19.02.2024');
+
+INSERT INTO Shtrikker
+VALUES
+('Dmytro', 12, '19.02.2024');
