a1b3225ec7574b25248ba1dd8781857604868246
Ilya Runcev <keneyz057@gmail.com>
2024-02-25 23:05:15 +0200

    Homework

---
+++ c_cxx/lesson3/Homework_IK/homework_IK.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int sum(int n){
+    int sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        sum = sum + (i + 1);
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+
+    do {
+        std::cout << "Enter value for n(n >= 0): ";
+        std::cin >> n;
+    } while (!(n >= 0));
+    int result = sum(n);
+    std::cout << " sum(n,x) = " << sum(n);
+
+    return 0;
+}
